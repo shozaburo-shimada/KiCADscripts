2fc69f5796d34c244b4e167676f114c21a222586
Shozaburo Shimada <shozaburo.s@gmail.com>
2018-05-17 08:57:34 +0900

    bug fix

--- changeRef.py
+++ changeRef.py
@@ -3,19 +3,22 @@ import pcbnew
 board = pcbnew.GetBoard()
 c = board.GetModules()
 for x in c:
-    ##print x.GetReference()
     ref = x.GetReference()
+    #print "#Ref: ", ref
     j = 0
     for p in range(len(ref)):
         if(ref[p].isalpha()!=True):
+            #print "ref[p]: ", ref[p]
             if(ref[p]=="*"):
                 break
             else:
                 i = ref[0:p]
                 j = int(ref[p:])
+                break
     if(j>0):
-        k = i + '{0:04d}'.format(j)
-        # k=i+‘{:d}’.format(j+100)
+        #print "i: ", i, ", j: ", j
+        #k = i + '{0:04d}'.format(j)
+        k=i+'{:d}'.format(j+150)
         print ref, "->", k
         ## remove next comment and run!
         ##x.SetReference(k)
