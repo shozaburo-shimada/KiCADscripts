daf2fe35cc6f6b2348620bc654dfaad9500e1024
Shozaburo Shimada <shozaburo@piccolo.co>
2018-05-24 14:39:01 +0900

    update

--- changeAttribute.py
+++ changeAttribute.py
@@ -1,9 +1,24 @@
 import pcbnew
 
 board = pcbnew.GetBoard()
-c = board.GetModules()
-for x in c:
-    ref = x.GetReference()
-    attr = x.GetAttributes()
-    print "#Ref: ", ref, " #Attrubute: ", attr
-    #x.SetAttributes(1)
+mlist = board.GetModules()
+for m in mlist:
+    ref = m.GetReference()
+    print "#Ref: ", ref
+
+    plist = m.Pads()
+    dip_flag = 1
+    for p in plist:
+        #print "pad: ", p.GetDrillSize()
+        if p.GetDrillSize()[0] == 0:
+            dip_flag = 0
+            break
+
+    if dip_flag == 0:
+        print "    This is SMD module"
+        m.SetAttributes(1)
+    else:
+        print "    This is DIP module"
+
+    attr = m.GetAttributes()
+    print  "    Attrubute: ", attr
